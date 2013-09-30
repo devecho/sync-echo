757c73a86b1431a2804109927f576b9e0b01c146
Daniel Manzke <daniel.manzke@googlemail.com>
2013-09-30 21:41:30 +0200

    changed to use new converter

--- se-service/src/main/java/de/devsurf/echo/sync/links/converter/LinkConverter.java
+++ se-service/src/main/java/de/devsurf/echo/sync/links/converter/LinkConverter.java
@@ -1,13 +1,11 @@
 package de.devsurf.echo.sync.links.converter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import de.devsurf.echo.frameworks.rs.api.TwoWayConverter;
 import de.devsurf.echo.sync.api.Field;
-import de.devsurf.echo.sync.api.FieldType;
 import de.devsurf.echo.sync.links.api.Link;
 import de.devsurf.echo.sync.links.persistence.LinkEntity;
 import de.devsurf.echo.sync.persistence.FieldEntity;
@@ -17,6 +15,9 @@ import de.devsurf.echo.sync.providers.persistence.ProviderPersistency;
 public class LinkConverter implements TwoWayConverter<LinkEntity, Link> {
 	@Inject
 	private ProviderPersistency providers;
+	
+	@Inject
+	private TwoWayConverter<List<FieldEntity>, List<Field>> fieldConverter;
 	
 	@Override
 	public LinkEntity convertFrom(Link source) {
@@ -28,16 +29,8 @@ public class LinkConverter implements TwoWayConverter<LinkEntity, Link> {
 		linkEntity.setProvider(provider);
 		
 		linkEntity.setUser(1);//TODO change to user object and make link converter aware of context
-		
-		List<Field> fields = source.getData();
-		List<FieldEntity> entities = new ArrayList<>();
-		for(Field field : fields) {
-			FieldEntity entity = new FieldEntity();
-			entity.setName(field.getName());
-			entity.setValue(field.getValue());
-			entities.add(entity);
-		}
-		linkEntity.setFields(entities);
+
+		linkEntity.setFields(fieldConverter.convertFrom(source.getData()));
 		
 		return linkEntity;
 	}
@@ -59,34 +52,7 @@ public class LinkConverter implements TwoWayConverter<LinkEntity, Link> {
 			
 			@Override
 			public List<Field> getData() {
-				List<Field> fields = new ArrayList<>();
-				List<FieldEntity> authFields = delegate
-						.getFields();
-				if (authFields != null) {
-					for (final FieldEntity field : authFields) {
-						Field authField = new Field() {
-							private FieldEntity delegate = field;
-
-							@Override
-							public String getValue() {
-								return delegate.getValue();
-							}
-
-							@Override
-							public FieldType getKind() {
-								return delegate.getType();
-							}
-
-							@Override
-							public String getName() {
-								return delegate.getName();
-							}
-						};
-						fields.add(authField);
-					}
-				}
-
-				return fields;
+				return fieldConverter.convertTo(delegate.getFields());
 			}
 		};
 	}
--- se-service/src/main/java/de/devsurf/echo/sync/providers/converter/ProviderConverter.java
+++ se-service/src/main/java/de/devsurf/echo/sync/providers/converter/ProviderConverter.java
@@ -1,12 +1,12 @@
 package de.devsurf.echo.sync.providers.converter;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import de.devsurf.echo.frameworks.rs.api.Converter;
 import de.devsurf.echo.frameworks.rs.api.Type;
-import de.devsurf.echo.sync.api.FieldType;
 import de.devsurf.echo.sync.providers.api.Provider;
 import de.devsurf.echo.sync.providers.api.ProviderAuthentication;
 import de.devsurf.echo.sync.providers.api.ProviderAuthenticationField;
@@ -15,6 +15,9 @@ import de.devsurf.echo.sync.providers.persistence.ProviderAuthenticationFieldEnt
 import de.devsurf.echo.sync.providers.persistence.ProviderEntity;
 
 public class ProviderConverter implements Converter<ProviderEntity, Provider> {
+	@Inject
+	private Converter<List<ProviderAuthenticationFieldEntity>, List<ProviderAuthenticationField>> fieldConverter;
+	
 	@Override
 	public Provider convertTo(final ProviderEntity source) {
 		return new Provider() {
@@ -63,43 +66,7 @@ public class ProviderConverter implements Converter<ProviderEntity, Provider> {
 
 					@Override
 					public List<ProviderAuthenticationField> getData() {
-						List<ProviderAuthenticationField> fields = new ArrayList<>();
-						List<ProviderAuthenticationFieldEntity> authFields = delegate
-								.getFields();
-						if (authFields != null) {
-							for (final ProviderAuthenticationFieldEntity field : authFields) {
-								ProviderAuthenticationField authField = new ProviderAuthenticationField() {
-									private ProviderAuthenticationFieldEntity delegate = field;
-
-									@Override
-									public String getValue() {
-										return delegate.getValue();
-									}
-
-									@Override
-									public FieldType getKind() {
-										return delegate.getType();
-									}
-
-									@Override
-									public String getName() {
-										return delegate.getName();
-									}
-									
-									@Override
-									public String getDescription() {
-										return delegate.getDescription();
-									}
-									
-									public boolean isOptional() {
-										return delegate.isOptional();
-									}
-								};
-								fields.add(authField);
-							}
-						}
-
-						return fields;
+						return fieldConverter.convertTo(delegate.getFields());
 					}
 				};
 			}
